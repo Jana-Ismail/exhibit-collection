c8d74b5ea0efd2e48df7aac06cc773902633ed5d
Jana Ismail <janaismail@janas-air.lan>
2024-07-23 13:38:35 -0500

    Set up /login and /register Routes in App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const container = document.getElementById("root")
+const root = ReactDOM.createRoot(container)
+root.render(
+  <BrowserRouter>
+    <App />  
+  </BrowserRouter>
 )
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )

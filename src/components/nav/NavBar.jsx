6a7056f09afbbc822855ec36eed874cc3dd24ad0
Jana Ismail <janaismail@janas-air.lan>
2024-07-23 14:43:40 -0500

    Added NavBar component for authorized user

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,1 +1,28 @@
+import { Link } from "react-router-dom"
 import "./NavBar.css"
+
+export const NavBar = () => {
+    return (
+        <ul className="navbar">
+            <li className="navbar-item">
+                <Link to="/collection">Collection</Link>
+            </li>
+            {localStorage.getItem("exhibit_user") ? (
+                <li className="navbar-item navbar-logout">
+                    <Link
+                        className="navbar-link"
+                        to=""
+                        onClick={() => {
+                            localStorage.removeItem("exhibit_user")
+                            navigate("/", { replace: true })
+                    }}
+                    >
+                        Logout
+                    </Link>
+                </li>
+                ) : (
+                ""
+                )}
+        </ul>
+    )
+}

1b0442c4420d1845bc340ca0d03f22bcf6c0a7dc
Jana Ismail <janaismail@janas-air.lan>
2024-08-12 12:25:52 -0500

    Fixed filter bar bug

--- src/components/artistUsers/ArtistArtworkList.jsx
+++ src/components/artistUsers/ArtistArtworkList.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react"
 import { getArtistArtworksByArtistId } from "../../services/artistUserService.js"
+import { useNavigate } from "react-router-dom"
 
 export const ArtistArtworkList = ({currentArtistUser}) => {
     const [artistArtworks, setArtistArtworks] = useState([])
+    const navigate = useNavigate()
 
     const getAndSetArtistArtworks = () => {
         getArtistArtworksByArtistId(currentArtistUser.id).then(artworksArr => {
@@ -18,6 +20,14 @@ export const ArtistArtworkList = ({currentArtistUser}) => {
         <>
             <div className="collection-header">
                 <h2 className="collection-title">My Artwork</h2>
+                <button 
+                        className="add-artwork-btn"
+                        onClick={() => {
+                            navigate("/collection/create")
+                        }}
+                    >
+                        Add Artwork
+                    </button>
             </div>
             <div className="artwork-collection">
         
